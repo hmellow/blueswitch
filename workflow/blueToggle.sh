67635e0fa01373fe478fc6876e98c24f62b4e03f
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-05-21 22:24:54 -0600

    ssh access

--- workflow/blueToggle.sh
+++ workflow/blueToggle.sh
@@ -1,19 +1,38 @@
 config_file="$HOME/blueswitch-config.txt"
 
-# First line of config is ssh IP
+# Read the first line of config as SSH IP
 read -r sshIP < "$config_file"
-# Remaining lines are intended devices
-mapfile -t devices < <(tail -n +2 "$config_file")
 
+# Read the second line of config as the username
+read -r sshUSER < <(sed -n '2p' "$config_file")
+
+# Read the remaining lines as an array of devices
+mapfile -t devices < <(tail -n +3 "$config_file")
+
+# Rest of the code remains the same...
 stat=$(/usr/local/bin/blueutil --is-connected "${devices[0]}")
 if [[ "$stat" = '1' ]]; then
-	for item in "${devices[@]}"; do
-		/usr/local/bin/blueutil --unpair "$item"
-	done
+    for item in "${devices[@]}"; do
+        /usr/local/bin/blueutil --unpair "$item"
+    done
 else
-	for item in "${devices[@]}"; do
-		/usr/local/bin/blueutil --unpair "$item"
-		/usr/local/bin/blueutil --pair "$item"
-		/usr/local/bin/blueitil --connect "$item"
-	done
+    for item in "${devices[@]}"; do
+        /usr/local/bin/blueutil --unpair "$item"
+        /usr/local/bin/blueutil --pair "$item"
+        /usr/local/bin/blueitil --connect "$item"
+    done
+fi
+sleep 1
+
+# Do it on the other device
+if [[ "$stat" = '1' ]]; then
+    for item in "${devices[@]}"; do
+        ssh "$sshUSER@$sshIP" "/usr/local/bin/blueutil --unpair \"$item\""
+    done
+else
+    for item in "${devices[@]}"; do
+        ssh "$sshUSER@$sshIP" "/usr/local/bin/blueutil --unpair \"$item\""
+        ssh "$sshUSER@$sshIP" "/usr/local/bin/blueutil --pair \"$item\""
+        ssh "$sshUSER@$sshIP" "/usr/local/bin/blueutil --connect \"$item\""
+    done
 fi
